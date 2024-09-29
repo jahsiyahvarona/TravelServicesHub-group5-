17efbbe1207ff0298addfa42dc5421a20c50c698
jahsiyah varona <jahsiyah@Jahsiyahs-MacBook-Air.local>
2024-09-29 12:40:33 -0400

    this is the start pages for our web app

---
+++ TSH app/TravelServicesHub/src/test/java/com/group5/TravelServicesHub/TravelServicesHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group5.TravelServicesHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelServicesHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
