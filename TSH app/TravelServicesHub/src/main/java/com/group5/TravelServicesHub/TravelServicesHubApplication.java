d1453a9d266fe1425c138d039755254ceccd5a53
jahsiyah varona <jahsiyah@Jahsiyahs-MacBook-Air.local>
2024-10-01 13:11:19 -0400

    Initial commit

---
+++ TSH app/TravelServicesHub/src/main/java/com/group5/TravelServicesHub/TravelServicesHubApplication.java
@@ -0,0 +1,13 @@
+package com.group5.TravelServicesHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelServicesHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelServicesHubApplication.class, args);
+	}
+
+}
